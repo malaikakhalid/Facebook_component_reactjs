46f5c0994fc48fbff966dac7d6f78d2de932167b
malaikakhalid <70518776+malaikakhalid@users.noreply.github.com>
2021-02-15 21:45:49 +0500

    facebook_component

---
+++ build/precache-manifest.9f9a0f9be39aa0e3403b665847e1bd9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f0bf091b51001239e68e2a6d685500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ce9fbaae6dcd51e8888",
+    "url": "/static/css/2.c01ca395.chunk.css"
+  },
+  {
+    "revision": "f9aa1f2cdc771461c089",
+    "url": "/static/css/main.64d3dcc8.chunk.css"
+  },
+  {
+    "revision": "1ce9fbaae6dcd51e8888",
+    "url": "/static/js/2.fb18194d.chunk.js"
+  },
+  {
+    "revision": "05f004cf1ac1b697d59f67cd14a990d6",
+    "url": "/static/js/2.fb18194d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9aa1f2cdc771461c089",
+    "url": "/static/js/main.34fa7f94.chunk.js"
+  },
+  {
+    "revision": "2b3c6c12f578ca045fe3",
+    "url": "/static/js/runtime-main.87f6ccef.js"
+  }
+]);
